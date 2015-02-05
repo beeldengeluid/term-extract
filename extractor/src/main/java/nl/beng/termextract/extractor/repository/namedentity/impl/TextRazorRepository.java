f7a1a8f0749333bbf6011ea8d86a0f1c76bb03de
Bas de Vos <bas.de.vos@42.nl>
2015-02-05 14:29:22 +0100

    Excluded TextRazor entities without type and set MISC when unmapped type.

--- extractor/src/main/java/nl/beng/termextract/extractor/repository/namedentity/impl/TextRazorRepository.java
+++ extractor/src/main/java/nl/beng/termextract/extractor/repository/namedentity/impl/TextRazorRepository.java
@@ -3,6 +3,7 @@ package nl.beng.termextract.extractor.repository.namedentity.impl;
 import static java.util.ResourceBundle.getBundle;
 import static nl.beng.termextract.extractor.repository.namedentity.NamedEntityType.MISC;
 import static nl.beng.termextract.extractor.repository.namedentity.NamedEntityType.valueOf;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import nl.beng.termextract.extractor.repository.namedentity.NamedEntity;
 import nl.beng.termextract.extractor.repository.namedentity.NamedEntityExtractionException;
 import nl.beng.termextract.extractor.repository.namedentity.NamedEntityRecognitionRepository;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -23,6 +25,7 @@ import com.textrazor.annotations.Entity;
 @Repository
 public class TextRazorRepository implements NamedEntityRecognitionRepository {
     public static final ResourceBundle ENTITY_MAP = getBundle("textrazor_entities");
+    private static final Logger LOG = getLogger(TextRazorRepository.class);
 
     @Autowired
     private TextRazor client;
@@ -34,7 +37,9 @@ public class TextRazorRepository implements NamedEntityRecognitionRepository {
             List<Entity> textRazorEntities = client.analyze(text).getResponse().getEntities();
             if (textRazorEntities != null) {
                 for (Entity entity : textRazorEntities) {
-                    foundEntities.add(convert(entity));
+                    if (entity.getType() != null) {
+                        foundEntities.add(convert(entity));
+                    }
                 }
             }
         } catch (NetworkException | AnalysisException e) {
@@ -51,20 +56,19 @@ public class TextRazorRepository implements NamedEntityRecognitionRepository {
         if (convertedType != null) {
             namedEntity.setType(valueOf(convertedType));
         } else {
-            throw new NamedEntityExtractionException("No GTAA type found for any of TextRazor types: " + entity.getType(), null);
+            namedEntity.setType(MISC);
+            LOG.warn("No GTAA type found for any of TextRazor types: " + entity.getType() + " setting default type MISC...");
         }
         return namedEntity;
     }
 
     private String convertType(List<String> types) {
         String convertedType = null;
-        if (types != null) {
-            for (String type : types) {
-                if (ENTITY_MAP.containsKey(type)) {
-                    convertedType = ENTITY_MAP.getString(type);
-                    if (valueOf(convertedType) != MISC) {
-                        break;
-                    }
+        for (String type : types) {
+            if (ENTITY_MAP.containsKey(type)) {
+                convertedType = ENTITY_MAP.getString(type);
+                if (valueOf(convertedType) != MISC) {
+                    break;
                 }
             }
         }
